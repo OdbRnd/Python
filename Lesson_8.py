9227c4aebbfdea437e2178582b8cc7b95470aba7
OdbRnd <124455744+OdbRnd@users.noreply.github.com>
2024-05-24 19:25:27 +0300

    Add files via upload

---
+++ Lesson_8.py
@@ -0,0 +1,43 @@
+# Задание № 1
+
+# def StrAge(age):
+   
+#     if (age > 4) and (age < 21):
+#        sStr =' лет.'
+#     else:
+#        i = age % 10
+#        if i==1:
+#           sStr =' год.'
+#        elif i < 5:
+#           sStr = ' года.'
+#        else:
+#           sStr = ' лет.'
+#     return(sStr)
+   
+# pets = dict()
+# lFl = True
+# while lFl:
+#    sName = str(input("Введите имя питомца (Enter - выход): "))
+#    if sName:
+#        pets[sName] = {}
+#        sStr = str(input("Введите вид питомца: "))
+#        pets[sName]["Вид_питомца"] = sStr
+#        nVoz = int(input("Введите возраст питомца: "))
+#        pets[sName]["Возраст_питомца"] = nVoz
+#        sChar = str(input("Введите имя владельца: "))
+#        pets[sName]["Имя_владельца"] = sChar
+#    else:
+#        lFl = False
+# for i in pets.keys():
+#    dData = list(pets[i].values())
+#    print("Это", dData[0].strip(), 'по кличке "', i.strip(), '". Возраст питомца:', dData[1], StrAge(dData[1]),
+#          "Имя владельца:", dData[2].strip() )
+
+# Задание № 2
+nNum1, nNum2 = map(int, input("Начальное и конечное целые числа в строке: ").split())
+nDic = dict()
+if nNum1 < nNum2:  nStep = 1
+else: nStep = -1
+for i in range(nNum1, nNum2+nStep, nStep):
+    nDic[i] = i**i
+print(nDic)
