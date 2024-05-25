ec3d9c50a28955e6d7314bca94f65c1411a92cfe
OdbRnd <124455744+OdbRnd@users.noreply.github.com>
2024-05-25 11:21:58 +0300

    Add files via upload

--- Lesson_8.py
+++ Lesson_8.py
@@ -14,26 +14,48 @@
 #           sStr = ' лет.'
 #     return(sStr)
    
-# pets = dict()
-# lFl = True
-# while lFl:
-#    sName = str(input("Введите имя питомца (Enter - выход): "))
-#    if sName:
-#        pets[sName] = {}
-#        sStr = str(input("Введите вид питомца: "))
-#        pets[sName]["Вид_питомца"] = sStr
-#        nVoz = int(input("Введите возраст питомца: "))
-#        pets[sName]["Возраст_питомца"] = nVoz
-#        sChar = str(input("Введите имя владельца: "))
-#        pets[sName]["Имя_владельца"] = sChar
-#    else:
-#        lFl = False
+pets = dict()
+lFl = True
+while lFl:
+    sName = str(input("Введите имя питомца (Enter - выход): "))
+    if sName:
+        pets[sName] = {}
+        sStr = str(input("Введите вид питомца: "))
+        pets[sName]["Вид_питомца"] = sStr
+        nVoz = int(input("Введите возраст питомца: "))
+        pets[sName]["Возраст_питомца"] = nVoz
+        sChar = str(input("Введите имя владельца: "))
+        pets[sName]["Имя_владельца"] = sChar
+    else:
+        lFl = False
+
+# версия 1 с функцией
 # for i in pets.keys():
 #    dData = list(pets[i].values())
-#    print("Это", dData[0].strip(), 'по кличке "', i.strip(), '". Возраст питомца:', dData[1], StrAge(dData[1]),
-#          "Имя владельца:", dData[2].strip() )
+#    print("Это", dData[0].strip(), 'по кличке "', i.strip(), '". Возраст питомца:', dData[1], 
+#          StrAge(dData[1]), "Имя владельца:", dData[2].strip() )
 
+# версия 2 без функции
+for i in pets.keys():
+   sStr = ""
+   dData = list(pets[i].values())
+   if (dData[1] > 4) and (dData[1] < 21):
+      sStr =' лет.'
+   else:
+      age = dData[1] % 10
+      if age==1:
+         sStr =' год.'
+      elif age < 5:
+         sStr = ' года.'
+      else:
+         sStr = ' лет.'
+
+   print("Это", dData[0].strip(), 'по кличке "', i.strip(), '". Возраст питомца:', dData[1], 
+         sStr+" Имя владельца:", dData[2].strip() )
+
+# --------------------------------------------------------------------------------------------------------
 # Задание № 2
+
 nNum1, nNum2 = map(int, input("Начальное и конечное целые числа в строке: ").split())
 nDic = dict()
 if nNum1 < nNum2:  nStep = 1
